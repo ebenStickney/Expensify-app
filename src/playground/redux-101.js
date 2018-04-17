925df9a2cf2b8d05ff78d73d38147a697b255860
ebenStickney <36087626+ebenStickney@users.noreply.github.com>
2018-04-17 15:29:51 -0700

    Dynamic Action examples
    Using count as an example of how to change our Redux state with actions.

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,66 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = {count: 0} ) => {
-    return state;
+const store = createStore((state = {count: 0},action ) => {
+    switch (action.type) {
+        case "INCREMENT": 
+          const incrementBy = typeof action.incrementBy === 'number'? action.incrementBy : 1;
+          return {
+           count: state.count + incrementBy
+       };
+        break;
+        case "DECREMENT":
+          const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+          return {
+           count: state.count - decrementBy
+          };
+        break;
+        case "RESET": 
+          return {
+           count: 0
+            };
+        break;
+        default: 
+          return state;
+    }
 });
 
 console.log(store.getState());
+
+store.dispatch({
+    type: "INCREMENT",
+    incrementBy: 5
+});
+
+console.log(store.getState());
+
+store.dispatch({
+    type: "INCREMENT"
+});
+
+console.log(store.getState());
+
+store.dispatch({
+    type: "INCREMENT"
+});
+
+console.log(store.getState());
+
+store.dispatch({
+    type: "DECREMENT",
+    decrementBy: 3
+});
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "RESET"
+});
+
+console.log(store.getState());
+
+store.dispatch({
+    type: "INCREMENT"
+});
+
+console.log(store.getState());
+
