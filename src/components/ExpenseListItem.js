938b1837f8718adbf17be96b3bf39ed2132f41de
ebenStickney <36087626+ebenStickney@users.noreply.github.com>
2018-04-23 11:15:44 -0700

    UI basics
    Remove button and text filter.

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { removeExpense } from '../actions/expenses';
+import { connect } from 'react-redux'; 
 
-const ExpenseListItem = ( {description, amount, createdAt} ) => (
+
+const ExpenseListItem = ({dispatch, id , description, amount, createdAt} ) => (
     <div>
      <h4> {description} </h4>
      <p>Amount: {amount} -- Created at: {createdAt}</p>
+      <button onClick={ ()=> {
+    dispatch(removeExpense( {id} ))
+            } }
+      > Remove </button>
     </div>
 
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
