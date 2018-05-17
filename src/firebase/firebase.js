79653b7cdbe688420e2a9692ea7f79e2c8060220
ebenStickney <36087626+ebenStickney@users.noreply.github.com>
2018-05-16 21:08:56 -0700

    managing database testing CRUD
    messing around with manipulation and fetching.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,7 +9,56 @@ const config = {
     messagingSenderId: "202787155255"
   };
 
+
   firebase.initializeApp(config);
-  firebase.database().ref().set({
-    testing: 'is working'
+
+
+  const database = firebase.database();
+
+  database.ref('traits/dopeness').set('eh..okay')
+
+  database.ref().on('value', (snapshot) => {
+    const val = snapshot.val();
+    console.log(`${val.name} could be described as ${val.traits.dopeness}.`);
   });
+
+ setTimeout( () => {
+   database.ref().update({'traits/dopeness': 'dope, level midnight'});
+ }, 7000);
+
+  // database.ref()
+  // .once('value')
+  // .then( (snapshot) => {
+  //   const val = snapshot.val();
+  //   console.log(val);
+  // })
+  // .catch( (e) => {
+  //   console.log('error:', e);
+  // });
+  //
+  // database.ref().set({
+  //   name: 'Eben',
+  //   age: 33,
+  //   location: {
+  //     city: 'Portland',
+  //     state: 'Oregon'
+  //   }
+  // })
+  // .then( () => {console.log('data passed')})
+  // .catch( (e) => {console.log('error', e)});
+  //
+  //
+  // // database.ref('traits').set({
+  // //   funny: true,
+  // //   single: false,
+  // //   desireToCode: 10
+  // // }).then( () => {console.log('data passed test!')})
+  // // .catch( (e) => {console.log('error!!', e)});
+  // //
+  // // database.ref('traits/single').remove()
+  // // .then( () => {console.log('Yo shit is gone')})
+  // // .catch( (e) => { console.log('naw, brah', e)});
+  // //
+  // // database.ref().update({'location/state': 'Maine', 'traits/desireToCode': 11})
+  // // .then( () => {console.log('Yo shit is updated')})
+  // // .catch( (e) => { console.log('naw, brah', e)});
