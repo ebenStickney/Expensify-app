4b7fc0a9eb3485c89ea8ce0c3c449bfe3d387984
ebenStickney <36087626+ebenStickney@users.noreply.github.com>
2018-05-18 12:57:13 -0700

    testing expense add

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,5 +1,60 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
+
+
+const createMockStore = configureMockStore([thunk]);
+
+test('should add expense to database and store', (done) => {
+  const store = createMockStore({});
+  const expenseData = {
+    description: 'toothpick',
+    amount: 10000,
+    note: 'made from real unicorn',
+    createdAt: 304928483939
+  };
+  store.dispatch(startAddExpense(expenseData)).then( ()=> {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'ADD_EXPENSE',
+      expenses: {
+        id: expect.any(String),
+        ...expenseData
+      }
+    });
+    return database.ref(`expenses/${actions[0].expenses.id}`).once('value');
+  }).then( (snapshot) => {
+    expect(snapshot.val()).toEqual(expenseData);
+    done();
+  })
+
+});
+
+test('should add expense with defaults to the store and DB', (done) => {
+  const store = createMockStore({});
+  const expenseDefaults = {
+    description: '',
+    amount: 0,
+    note: '',
+    createdAt: 0
+  };
+  store.dispatch(startAddExpense({})).then( ()=> {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'ADD_EXPENSE',
+      expenses: {
+        id: expect.any(String),
+        ...expenseDefaults
+      }
+    });
+    return database.ref(`expenses/${actions[0].expenses.id}`).once('value');
+  }).then( (snapshot) => {
+    expect(snapshot.val()).toEqual(expenseDefaults);
+    done();
+  })
+})
 
 test('should set up remove expense action object', () => {
     const action = removeExpense( { id: '123abc' } );
